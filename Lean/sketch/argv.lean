459000ecf9305538639acf1d4c295ae7e6d3337e
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2024-05-04 11:35:58 +0800

    failed to obtain the `mainModule` at runtime

--- Lean/sketch/argv.lean
+++ Lean/sketch/argv.lean
@@ -1,10 +1,18 @@
--- An entry script should not be defined in a namespace
+import Lean.CoreM
+import Lean.MonadEnv
+import Lean.Environment
 
+---------------------------------------------------------------
+#eval show Lean.CoreM Unit from do
+  dbg_trace s!"running: {(<- Lean.getEnv).header.mainModule}"
+
+---------------------------------------------------------------
 def main (args : List String) : IO UInt32 := do
-  let self <- IO.appPath
+  let pwd <- IO.currentDir
+  let arguments := s!"argument{if args.length == 1 then "" else "s"}"
 
-  IO.println s!"running: {self}"
-  IO.println s!"  received {args.length} argument{if args.length == 1 then "" else "s"} from user:"
+  IO.println s!"  in: {pwd}"
+  IO.println s!"  received {args.length} {arguments} from user:"
 
   for arg in args,
       idx in List.range args.length
